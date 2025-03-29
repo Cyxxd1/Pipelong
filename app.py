b644947ec60b3d128c4031d8f7f88002aae88fa0
Alvin-Benny <alvinbenny2004@gmail.com>
2025-03-29 21:14:18 +1000

    committed site changes
    added logo and updated readme

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-
+    
 @app.route("/dashboard")
 def dashboard():
     return render_template("index.html")
